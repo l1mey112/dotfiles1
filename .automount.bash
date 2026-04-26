9b9d478a6bb36cba060e35e6021c859484aa517e
l-m <l-m@l-m.dev>
2026-04-26 12:01:15 +1000

    automount

---
+++ .automount.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+pidfile=/tmp/automount.pid
+
+if [[ -e $pidfile ]] && kill -0 "$(cat $pidfile)" 2>/dev/null; then
+    echo "already running" && exit 1
+fi
+
+# permit persist keepenv :wheel as root
+# permit nopass :wheel as root cmd mount
+# permit nopass :wheel as root cmd umount
+
+echo $$ > $pidfile
+trap "rm -f $pidfile" EXIT
+
+udevadm monitor --udev --subsystem-match=block | while read -r _ _ action dev _; do
+    [[ $action != add ]] && continue
+    node=/dev/$(basename "$dev")
+	doas mount -o rw,umask=000 "$node" ~/mnt2 && notify-send "mounted $node"
+done
+
